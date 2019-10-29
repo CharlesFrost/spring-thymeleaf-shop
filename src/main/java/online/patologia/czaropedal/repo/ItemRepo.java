967746ad90bc3de2455d4c73c0885c4b5f216fcf
Karol <mroziaczek00@gmail.com>
2019-10-30 00:41:03 +0100

    refactoring

--- src/main/java/online/patologia/czaropedal/repo/ItemRepo.java
+++
@@ -1,9 +0,0 @@
-package online.patologia.czaropedal.repo;
-
-import online.patologia.czaropedal.model.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepo extends JpaRepository<Item,Long> {
-}
